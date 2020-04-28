743d00ae7ecce6947bfc63921aacf84d86513490
Henrynunez112 <henrynunez@pursuit.org>
2020-04-28 13:47:07 -0400

    small edit

--- calculator/src/components/Calculator/Calculator.js
+++ calculator/src/components/Calculator/Calculator.js
@@ -4,14 +4,14 @@ import Keypad from "./Keypad/Keypad";
 import { evaluate } from "mathjs";
 
 const Calculator = () => {
-  const initialState = 0
+  const initialState = 0;
   const [result, setResult] = useState(initialState);
   const [equation, setEquation] = useState("");
 
   const pressedBtn = (e) => {
     e.preventDefault();
     const btnValue = e.target.innerText;
-    let mutateEqua = [...equation]
+    let mutateEqua = [...equation];
     if (btnValue === "C") return clear();
     else if ((btnValue >= "0" && btnValue <= "9") || btnValue === ".") {
       setEquation(equation + btnValue);
@@ -22,18 +22,18 @@ const Calculator = () => {
         const fullEquation = evaluate(equation);
         if (!Number.isInteger(fullEquation)) {
           fullEquation.toFixed(2);
-          setResult(fullEquation)
+          setResult(fullEquation);
         } else {
           setResult(fullEquation);
           setEquation("");
         }
-      }catch(err){
-        alert('Yikes!!!');
-        setEquation("")
+      } catch (err) {
+        alert("This is not the way we do math!!!");
+        setEquation("");
         setResult(0);
       }
-    }else{
-      mutateEqua.pop()
+    } else {
+      mutateEqua.pop();
       setEquation(mutateEqua);
     }
   };

426114e0c2dc09ced536528dce0627fa0d905eed
Henrynunez112 <henrynunez@pursuit.org>
2020-04-27 22:04:46 -0400

    working with props

--- calculator/src/components/Calculator/Calculator.js
+++ calculator/src/components/Calculator/Calculator.js
@@ -3,25 +3,14 @@ import Screen from "../Screen/Screen";
 import Keypad from "./Keypad/Keypad";
 
 const Calculator = () => {
-  const [result, setResult] = useState(0);
-  const [equation, setEquation] = useState('');
-
-  const onButtonPress = (e) => {
-    debugger
-    let pressedButton = e.target.value;
-    if(pressedButton === 'C'){
-      return setResult(0) && setEquation('');
-    }else if((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.'){
-      setEquation(equation + pressedButton)
-    }else if(['+', '-', '*', '/', '%']){
-
-    }
-  };
+  const initialCount = 0;
+  const [result, setResult] = useState(initialCount);
+  const [equation, setEquation] = useState("");
 
   return (
     <main className="calculator">
       <Screen equation={equation} result={result} />
-      <Keypad onClick={() => onButtonPress()} />
+      <Keypad />
     </main>
   );
 };
